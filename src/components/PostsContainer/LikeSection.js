126863bb83f082d301ffeaf87811ff37132bff15
cladams0203 <cladams0203@gmail.com>
2020-03-17 14:00:02 -0400

    finished functionality mvp

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -4,7 +4,6 @@
 import React from "react";
 
 const LikeSection = props => {
-  console.log(props)
   return (
     <div>
     <div
@@ -12,14 +11,14 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart" onClick={() => props.setLikes(props.likes + 1)}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
     <p className="like-number">
-      
+      {props.likes} :
       likes</p>
 </div>
   )
